bf49b6f22809a6356653c5baea5765beb0f39d56
nicholas <nicho.tjornelund@gmail.com>
2025-11-08 21:00:23 +0100

    working hover

--- example/main.c
+++ example/main.c
@@ -30,55 +30,47 @@ int main() {
 
     int padding = (50-32)/2;
 
-    dk_ui_start_box(ctx, 0, 0);
-        dk_ui_box_float(ctx, FLOAT_NONE);
-
-        dk_ui_start_box(ctx, 0, 0);
-            dk_ui_box_float(ctx, FLOAT_LEFT);
-            dk_ui_box_set_gap(ctx, 32);
-
-            dk_ui_node *b1 = dk_ui_rect(ctx, 50, 50, (dk_color){1,1,1,0});
-            dk_ui_node *b2 = dk_ui_rect(ctx, 50, 50, (dk_color){1,1,1,0});
-            dk_ui_node *b3 = dk_ui_rect(ctx, 50, 50, (dk_color){1,1,1,0});
-
-        dk_ui_end_box(ctx);
-
-        dk_ui_start_box(ctx, 0, 0);
-            dk_ui_box_float(ctx, FLOAT_LEFT);
-            dk_ui_box_set_padding(ctx,padding,padding,padding,padding);
-            dk_ui_box_set_gap(ctx, 50);
-
-            dk_ui_texture(ctx, fedora_tex, 32, 32);
-            dk_ui_texture(ctx, fedora_tex, 32, 32);
-            dk_ui_texture(ctx, fedora_tex, 32, 32);
-        dk_ui_end_box(ctx);
-
-    dk_ui_end_box(ctx);
-    //evaluate_positions(ctx->node_mngr.root, 0, 0);
+    dk_color box_normal = (dk_color){1,1,1,0};
+    dk_color box_hover = (dk_color){1,1,1,0.2};
 
     // --- Render loop ---
     while (display_dispatch_blocking() != -1) {
         //printf("Mouse: %f, %f\n", mouse_info->mouse_x, mouse_info->mouse_y);
         dk_begin_frame(ctx);
-        dk_ui_draw(ctx, 0, 0);
 
+        dk_ui_reset(ctx);
+        dk_ui_start_box(ctx, 0, 0);
+            dk_ui_box_float(ctx, FLOAT_NONE);
+
+            dk_ui_start_box(ctx, 0, 0);
+                dk_ui_box_float(ctx, FLOAT_LEFT);
+                dk_ui_box_set_gap(ctx, 32);
+
+                dk_ui_node *b1 = dk_ui_rect(ctx, 50, 50, box_normal);
+                dk_ui_node *b2 = dk_ui_rect(ctx, 50, 50, box_normal);
+                dk_ui_node *b3 = dk_ui_rect(ctx, 50, 50, box_normal);
+
+            dk_ui_end_box(ctx);
+
+            dk_ui_start_box(ctx, 0, 0);
+                dk_ui_box_float(ctx, FLOAT_LEFT);
+                dk_ui_box_set_padding(ctx,padding,padding,padding,padding);
+                dk_ui_box_set_gap(ctx, 50);
+
+                dk_ui_texture(ctx, fedora_tex, 32, 32);
+                dk_ui_texture(ctx, fedora_tex, 32, 32);
+                dk_ui_texture(ctx, fedora_tex, 32, 32);
+            dk_ui_end_box(ctx);
+
+        dk_ui_end_box(ctx);
+        evaluate_positions(ctx->node_mngr.root, 0, 0);
         hit_query(ctx, mouse_info->mouse_x, mouse_info->mouse_y);
 
-        if(b1->hovered){
-            b1->data.color = (dk_color){1,1,1,0.2};
-            dk_draw_node(ctx,b1);
-        }
+        if(b1->hovered) b1->data.color.a = 1;
+        if(b2->hovered) b2->data.color.a = 1;
+        if(b3->hovered) b3->data.color.a = 1;
 
-        if(b2->hovered){
-            b1->data.color = (dk_color){1,1,1,0.2};
-            dk_draw_node(ctx,b1);
-        }
-
-        if(b3->hovered){
-            b1->data.color = (dk_color){1,1,1,0.2};
-            dk_draw_node(ctx,b1);
-        }
-
+        dk_ui_draw(ctx, 0, 0);
         dk_end_frame();
         egl_swap_buffers();
     }
